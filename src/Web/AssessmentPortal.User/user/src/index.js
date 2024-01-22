0147a6f9bc2ce9233f37e7d669c3a1c7acd05bef
Rajnesh Durai Thanga Raja <rajneshdurai.thangaraja@kanini.com>
2024-01-22 15:41:15 +0530

    Added AuthConfiguration

--- src/Web/AssessmentPortal.User/user/src/index.js
+++ src/Web/AssessmentPortal.User/user/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { PublicClientApplication } from "@azure/msal-browser";
+import { authConfig } from './authConfig'
+import { MsalProvider } from "@azure/msal-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const pca = new PublicClientApplication(authConfig);
 root.render(
+    <MsalProvider instance={pca}>
     <App />
+    </MsalProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
