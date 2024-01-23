c7fb0b8249eeac229a67ff78b06c363099033350
Rajnesh Durai Thanga Raja <rajneshdurai.thangaraja@kanini.com>
2024-01-23 18:16:11 +0530

    Got AccessToken from Auth

--- src/Web/AssessmentPortal.User/user/src/authConfig.js
+++ src/Web/AssessmentPortal.User/user/src/authConfig.js
@@ -1,65 +1,62 @@
-import * as Msal from "msal";
-import { BrowserCacheLocation, LogLevel } from "@azure/msal-browser";
-import * as jwtDecode from 'jwt-decode';
+import * as Msal from 'msal';
+import { BrowserCacheLocation, LogLevel } from '@azure/msal-browser';
 
 export const authConfig = {
   auth: {
-    clientId: "cb8f9a83-c59c-43b4-acba-88f8f6023434",
-    authority: "https://login.microsoftonline.com/common",
-    tenantId: "61330b40-bb04-4d25-b959-f3700fbe6023",
-    redirectUri: "https://code-assessment-platform.vercel.app/dashboard",
+    clientId: 'cb8f9a83-c59c-43b4-acba-88f8f6023434',
+    authority: 'https://login.microsoftonline.com/common',
+    tenantId:'61330b40-bb04-4d25-b959-f3700fbe6023',
+    redirectUri: 'https://code-assessment-platform.vercel.app/dashboard'
   },
   cache: {
-    cacheLocation: BrowserCacheLocation.LocalStorage,
+    cacheLocation: BrowserCacheLocation.SessionStorage,
     storeAuthStateInCookie: true,
   },
   system: {
     loggerOptions: {
-      loggerCallback: (level, message, containsPii) => {
-        if (containsPii) return;
-        level = LogLevel.Verbose;
+        loggerCallback: (level, message, containsPii) => {
+            if (containsPii) return;
+            level = LogLevel.Verbose;
 
-        switch (level) {
-          case LogLevel.Error:
-            console.error(message);
-            return;
-          case LogLevel.Info:
-            console.info(message);
-            return;
-          case LogLevel.Verbose:
-            console.debug(message);
-            return;
-          case LogLevel.Warning:
-            console.warn(message);
-            return;
-          default:
-            console.log(message);
+            switch (level) {
+                case LogLevel.Error:
+                    console.error(message);
+                    return;
+                case LogLevel.Info:
+                    console.info(message);
+                    return;
+                case LogLevel.Verbose:
+                    console.debug(message);
+                    return;
+                case LogLevel.Warning:
+                    console.warn(message);
+                    return;
+                default:
+                    console.log(message);
+            }
         }
-      },
-    },
-  },
+    }
+}
 };
 
 const msalInstance = new Msal.UserAgentApplication(authConfig);
 
 export const request = {
-  scopes: ["user.read"],
+  scopes: ['user.read'],
 };
 
 export function signIn() {
-  msalInstance
-    .loginPopup(request)
-    .then((response) => {
-      console.log("Login success:", response);
-      const jwtToken = response.accessToken
-      const decodedToken = jwtDecode(jwtToken);
-      console.log(decodedToken);
-      localStorage.setItem("accessToken", response.accessToken);
-      localStorage.setItem("email", decodedToken.upn);
-      localStorage.setItem("username", decodedToken.name);
+  msalInstance.loginPopup(request)
+    .then(response => {
+      console.log('Login success:', response);
+        //   const idToken = loginResponse.accessToken;
+  //   console.log(idToken);
+  //   sessionStorage.setItem('accessToken', idToken);
+  //   sessionStorage.setItem('email', loginResponse.account.username);
+  //   sessionStorage.setItem('username', loginResponse.account.name);
     })
-    .catch((error) => {
-      console.error("Login error:", error);
+    .catch(error => {
+      console.error('Login error:', error);
     });
 }
 
@@ -67,17 +64,7 @@ export function signOut() {
   msalInstance.logout();
 }
 
-function getUserProfile() {
-  msalInstance
-    .acquireTokenSilent(request)
-    .then((response) => {
-      console.log("Token acquired:", response);
-      // Use the token to call Microsoft Graph API or perform other actions
-    })
-    .catch((error) => {
-      console.error("Token acquisition error:", error);
-    });
-}
+
 
 // Example usage
 // signIn(); // Trigger the sign-in process
