20e862dd0f8995dd8236379f9d80495f33e6de86
Rajnesh Durai Thanga Raja <rajneshdurai.thangaraja@kanini.com>
2024-02-01 18:20:31 +0530

    feat: Updated ExitPage

--- src/Web/AssessmentPortal.User/user/src/features/pages/exitAssessmentPage/ExitAssessmentPage.jsx
+++ src/Web/AssessmentPortal.User/user/src/features/pages/exitAssessmentPage/ExitAssessmentPage.jsx
@@ -1,57 +1,62 @@
 import React, { useEffect } from "react";
-import KaniniBg from "../../../assets/bgkanini.png";
+import Kanini from "../../../assets/logo.png";
 import "../../../css/Global.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+ 
 const ExitAssessmentPage = () => {
+  const { width, height } = useWindowSize();
   useEffect(() => {
     // Hide scroll bar when the component mounts
     document.body.style.overflow = "hidden";
-
+ 
     // Show scroll bar when the component unmounts
     return () => {
       document.body.style.overflow = "visible";
     };
   }, []);
   return (
-    <div>
-      <img src={KaniniBg} className="kaninibg" alt="Kanini Background" />
-      <div className="exit-container">
-        <div className="align-center">
-          <FontAwesomeIcon icon={faCircleCheck} className="completed-image" />
-        </div>
-        <div className="text-align">
-          <h1 className="login-head">Thankyou For Taking the Assessment!!</h1>
-          <h3 className="login-head4">
-            Click on the below button to view your Result in Dashboard
-          </h3>
-        </div>
-        <div className="move-center">
-          <Link to="/dashboard">
-            <button class="cssbuttons-io-button">
-              Click Here
-              <div class="icon">
-                <svg
-                  height="24"
-                  width="24"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M0 0h24v24H0z" fill="none"></path>
-                  <path
-                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-              </div>
-            </button>
-          </Link>
+    <>
+      <div id="nav1">
+        <Confetti width={width} height={height} recycle={false} />
+        <div id="logo1">
+          <img src={Kanini} alt="Kanini logo." />
         </div>
       </div>
-    </div>
+      <div id="product-landing">
+        <div id="hero1">
+          <div id="hero-intro1">
+            <div id="hero-text">
+              <h1>
+                Kanini Assessment Portal
+              </h1>
+              <p>Thanking for taking the Assessment.</p>
+            </div>
+            <div id="hero-action-btn1">
+              <div id="apple1" class="hero-btn">
+                <Link to="/dashboard">
+                <div>
+    <button class="btn2"><i class="animation"></i>View Result<i class="animation"></i>
+    </button>
+</div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div id="hero-visual1">
+            <div id="hero-img">
+              <img
+                src="https://img.freepik.com/free-vector/employees-celebrating-business-success-with-huge-trophy_1150-37475.jpg?w=996&t=st=1706789532~exp=1706790132~hmac=2092e4cd1b68bbb091101360757d2ddee27c35ddb0b5943a5ccbd4020fa171fd"
+                alt="Completed"
+                id="testImage"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
-
+ 
 export default ExitAssessmentPage;
